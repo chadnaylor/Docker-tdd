f6d307483eec08517c93ec2a7b4805f31a70077d
Chad Naylor <chad.naylor@gmail.com>
2021-01-08 15:25:07 -0600

    test for frontend connection to api pass

--- libraryfrontend/src/App.js
+++ libraryfrontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react'
+//import Home from './components/home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      books: []
+    }
+  }
+  componentDidMount() {
+    return fetch(process.env.API_URL + '/api/books')
+      .then(response =>
+        response.json()
+      )
+      .then(data => this.setState({ books: data }))
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.books}
+        </header>
+
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- libraryfrontend/src/App.test.js
+++ libraryfrontend/src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
+//import app from '../../api/app'
+//import Home from './components/Home'
+
+describe('App', () => {
+  let appWrapper
+  beforeEach(() => {
+    appWrapper = shallow(<App />)
+  })
+  it('has state and is not empty', () => {
+    const State = appWrapper.state()
+    expect(State).not.toBeNull()
+  })
+
+  it('gets books from backend on mount', async () => {
+
+    const spy = jest.spyOn(App.prototype, 'componentDidMount');
+
+    await appWrapper.instance().componentDidMount();
+    expect(spy).toHaveBeenCalled();
+    appWrapper.update()
+    expect(appWrapper.state('books').length).toBeGreaterThan(0);
+
+
+    // const Home = appWrapper.find(Home)
+    // expect(Home.props().books).toEqual(State.books)
+  })
+
+
+
+
+
+})
